d65f50c383896ff4d34f76fd6c289fed31c58fd4
Saurav <167073133+Trafalgar-D-Water-Law-7@users.noreply.github.com>
2025-07-28 10:25:57 +0545

    Admin panel has been updated
    admin panel model has been updated

--- models/admin.js
+++ models/admin.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+
+const mongoose = require("mongoose")
 
 const adminSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
